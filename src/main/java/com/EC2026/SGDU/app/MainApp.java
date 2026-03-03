e46230f5c90ab801ccd682f1c5cd0651ac705dd4
Erick Bajaña <erick.bajana@epn.edu.ec>
2026-03-03 18:17:52 -0500

    Inicio del proyecto

---
+++ src/main/java/com/EC2026/SGDU/app/MainApp.java
@@ -0,0 +1,25 @@
+package com.EC2026.SGDU.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Carga el FXML desde resources
+        Parent root = FXMLLoader.load(getClass().getResource("/administracion/fxml/login.fxml"));
+
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Sistema de Gestión SGDU");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
